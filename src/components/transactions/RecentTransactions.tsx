629d953b42cb9f893d63c248c29f532b414aeb1e
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-03-28 16:48:23 +0100

    update: filter on transactions

--- src/components/transactions/RecentTransactions.tsx
+++ src/components/transactions/RecentTransactions.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchUserData } from "@/lib/api";
 import type { UnitTransaction } from "@/types/types";
 import { ArrowUp, ArrowDown } from "lucide-react";
 
 export default function RecentTransactions() {
   const [transactions, setTransactions] = useState<UnitTransaction[]>([]);
+  const [filter, setFilter] = useState<"all" | "income" | "expense">("all");
 
   const getUserTransaction = async () => {
     try {
@@ -22,47 +22,88 @@ export default function RecentTransactions() {
     getUserTransaction();
   }, []);
 
+  const filteredTransactions =
+    filter === "all"
+      ? transactions
+      : transactions.filter((transaction) =>
+          filter === "income"
+            ? transaction.type === "credit"
+            : transaction.type === "debit"
+        );
+
   return (
     <section className="flex flex-col gap-7 w-full">
-      <h2>Recent Transactions</h2>
+      <h2 className="font-semibold text-lg">Recent Transactions</h2>
+      <div className="flex gap-3 items-center justify-between font-medium">
+        <p
+          className={`pb-3 border-b-2 transition-all duration-300 ${
+            filter === "all"
+              ? "text-[#1814F3] border-b-[#1814F3]"
+              : "text-[#718EBF] border-b-transparent"
+          }`}
+          onClick={() => setFilter("all")}
+        >
+          All Transactions
+        </p>
+        <p
+          className={`pb-3 border-b-2 transition-all duration-300 ${
+            filter === "income"
+              ? "text-[#1814F3] border-b-[#1814F3]"
+              : "text-[#718EBF] border-b-transparent"
+          }`}
+          onClick={() => setFilter("income")}
+        >
+          Income
+        </p>
+        <p
+          className={`pb-3 border-b-2 transition-all duration-300 ${
+            filter === "expense"
+              ? "text-[#1814F3] border-b-[#1814F3]"
+              : "text-[#718EBF] border-b-transparent"
+          }`}
+          onClick={() => setFilter("expense")}
+        >
+          Expense
+        </p>
+      </div>
       <div className="flex flex-col gap-5">
-        {transactions &&
-          transactions.map((transaction, index) => {
-            let icon;
-            let color;
-            let sign;
-            if(transaction.type === "debit"){
-              icon = <ArrowUp />
-              color = "#FE5C73"
-              sign = "-"
-            }else{
-              icon = <ArrowDown />
-              sign = "+"
-              color = "#16DBAA";
-            }
+        {filteredTransactions.map((transaction, index) => {
+          let icon;
+          let color;
+          let sign;
+          if (transaction.type === "debit") {
+            icon = <ArrowUp className="w-[20px] hover:opacity-90" />;
+            color = "#FE5C73";
+            sign = "-";
+          } else {
+            icon = <ArrowDown className="w-[20px] hover:opacity-90" />;
+            sign = "+";
+            color = "#16DBAA";
+          }
 
-            return (
-              <div
-                key={index}
-                className="flex gap-3 items-center w-full justify-between"
-              >
-                <div className="flex gap-3 items-center">
-                  <div className="border w-10 h-10 rounded-full flex items-center justify-center">
-                    {icon}
-                  </div>
-                  <div>
-                    <p>{transaction.description}</p>
-                    <p>
-                      <span>
-                        {transaction.date}, {transaction.time}
-                      </span>
-                    </p>
-                  </div>
+          return (
+            <div
+              key={index}
+              className="flex gap-3 items-center w-full justify-between hover:opacity-90 cursor-pointer"
+            >
+              <div className="flex gap-3 items-center">
+                <div className="border w-[35px] h-[35px] rounded-full flex items-center justify-center border-[#718EBF] text-[#718EBF]">
+                  {icon}
                 </div>
-                <p className={`text-[${color}] font-medium`}>{sign}{transaction.amount}</p>
+                <div>
+                  <p className="font-medium">{transaction.description}</p>
+                  <p className="text-[#718EBF]">
+                    {transaction.date}, {transaction.time}
+                  </p>
+                </div>
               </div>
-            );
-          })}
+              <p className="font-medium" style={{ color }}>
+                {sign}
+                {transaction.amount}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
