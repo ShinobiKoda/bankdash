afe17e9433bb15c9170af25f53cfa0a8dd8ec461
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-04-03 13:10:22 +0100

    update: investment page design full implement

--- src/components/accounts/InvoiceSent.tsx
+++ src/components/accounts/InvoiceSent.tsx
@@ -88,7 +88,7 @@ export default function InvoiceSent() {
         </div>
       ) : (
         <motion.div
-          className="flex flex-col gap-4"
+          className="flex flex-col gap-4 h-full"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
