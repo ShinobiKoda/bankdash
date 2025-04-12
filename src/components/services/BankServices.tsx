989adde204da31303f56a2307a92e8c278c1c75c
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-04-12 21:49:13 +0100

    update: remove unused imports

--- src/components/services/BankServices.tsx
+++ src/components/services/BankServices.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
 import {
-  faShoppingBag,
-  faHelmetSafety,
+
   faShieldAlt,
   faMoneyBill,
   faMoneyBillTrendUp,
