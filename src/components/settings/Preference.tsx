654cb9a7611867883a1b5222a71df93c6ddc24e5
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-04-16 22:49:55 +0100

    update: mobile design implement

--- src/components/settings/Preference.tsx
+++ src/components/settings/Preference.tsx
@@ -1,9 +1,85 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 export default function Preference() {
+  const [digitalCurrencyToggle, setDigitalCurrencyToggle] = useState(false);
+  const [merchantOrderToggle, setMerchantOrderToggle] = useState(false);
+  const [recommendationsToggle, setRecommendationsToggle] = useState(false);
+
   return (
-    <div>
-      Preference
+    <div className="w-full">
+      <form className="flex flex-col gap-5 lg:flex-row justify-between">
+        <div className="flex flex-col gap-2 lg:flex-1">
+          <label htmlFor="currency">Currency</label>
+          <input
+            type="text"
+            placeholder="USD"
+            id="currency"
+            name="currency"
+            className="p-3 rounded-lg border border-[#DFEAF2]"
+          />
+        </div>
+        <div className="flex flex-col gap-2 lg:flex-1">
+          <label htmlFor="time-zone">Time Zone</label>
+          <input
+            type="text"
+            placeholder="GMT+0"
+            id="time-zone"
+            name="time-zone"
+            className="p-3 rounded-lg border border-[#DFEAF2]"
+          />
+        </div>
+      </form>
+      <div className="mt-5 flex flex-col gap-4">
+        <label className="flex items-center gap-3">
+          <div
+            className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${
+              digitalCurrencyToggle ? "bg-green-500" : "bg-gray-300"
+            }`}
+            onClick={() => setDigitalCurrencyToggle(!digitalCurrencyToggle)}
+          >
+            <div
+              className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform ${
+                digitalCurrencyToggle ? "translate-x-6" : "translate-x-0"
+              }`}
+            ></div>
+          </div>
+          <span>I send or receive digital currency</span>
+        </label>
+        <label className="flex items-center gap-3">
+          <div
+            className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${
+              merchantOrderToggle ? "bg-green-500" : "bg-gray-300"
+            }`}
+            onClick={() => setMerchantOrderToggle(!merchantOrderToggle)}
+          >
+            <div
+              className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform ${
+                merchantOrderToggle ? "translate-x-6" : "translate-x-0"
+              }`}
+            ></div>
+          </div>
+          <span>I receive merchant order</span>
+        </label>
+        <label className="flex items-center gap-3 lg:items-start">
+          <div
+            className={`min-w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${
+              recommendationsToggle ? "bg-green-500" : "bg-gray-300"
+            }`}
+            onClick={() => setRecommendationsToggle(!recommendationsToggle)}
+          >
+            <div
+              className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform ${
+                recommendationsToggle ? "translate-x-6" : "translate-x-0"
+              }`}
+            ></div>
+          </div>
+          <span className="lg:mt-1">
+            There are recommendations for my account
+          </span>
+        </label>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/settings/Security.tsx
+++ src/components/settings/Security.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { useState } from "react";
 
 export default function Security() {
+  const [twoFactor, setTwoFactor] = useState<boolean>(false);
+
   return (
-    <div>
-      Security
+    <div className="flex flex-col gap-5 w-full">
+      <h3>Two factor authentitaction</h3>
+      <div className="flex items-center gap-3 lg:items-start">
+        <div
+          className={`min-w-14 h-8 flex items-center rounded-full p-1 cursor-pointer ${
+            twoFactor ? "bg-green-500" : "bg-gray-300"
+          }`}
+          onClick={() => setTwoFactor(!twoFactor)}
+        >
+          <div
+            className={`w-6 h-6 bg-white rounded-full shadow-md transform transition-transform ${
+              twoFactor ? "translate-x-6" : "translate-x-0"
+            }`}
+          ></div>
+        </div>
+        <span className="lg:mt-1">Enable Two Factor Authentication</span>
+      </div>
+      <div>
+        <h3 className="mb-4">Change Password</h3>
+        <form action="" className="w-full flex flex-col gap-4">
+          <div className="w-full flex flex-col gap-2">
+            <label htmlFor="current-password">Current Password</label>
+            <input type="password" id="current-password" name="current-password" placeholder="********" className="p-3 border- border-[#DFEAF2] rounded-lg"/>
+          </div>
+          <div className="w-full flex flex-col gap-2">
+            <label htmlFor="new-password">New Password</label>
+            <input type="password" id="new-password" name="new-password" placeholder="********" className="p-3 border- border-[#DFEAF2] rounded-lg"/>
+          </div>
+          <button
+          type="submit"
+          className="py-3 w-full text-xl font-medium text-white bg-[#1814F3] my-5 rounded-xl border-none outline-none"
+        >
+          Save
+        </button>
+        </form>
+        <p></p>
+      </div>
     </div>
-  )
+  );
 }
